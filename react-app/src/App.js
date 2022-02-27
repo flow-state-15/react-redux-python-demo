df18fdbce843b4529f787bcb33bcdf4b59e9478c
flow-state-15 <purcellgroup88@gmail.com>
2022-02-27 11:15:18 -0600

    fixed footer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -50,7 +50,7 @@ function App() {
 
             <main className="App-main">
               <Gif />
-              {/* <Posts /> */}
+              <Posts />
             </main>
 
           </div>
--- react-app/src/components/Posts/index.js
+++ react-app/src/components/Posts/index.js
@@ -1,30 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
 
-// import { useDispatch, useSelector } from 'react-redux';
 
-// export default function Posts() {
-//   const dispatch = useDispatch();
-//   const gif_frame = useSelector((state) => state.frame);
+export default function Posts(){
+    const dispatch = useDispatch();
 
-//   //edge case
-//   const image = require(`../../assets/gif_frames_png/IMG00000.png`);
 
-//   const handle_click = () => {
-//     dispatch(increment_frame());
-//   };
-
-//   //posts logic
-//   const post_id = 1
-
-//   return (
-//     <div className="posts-wrapper">
-//           <p>Click buttons to change posts and add comments!</p>
-//           <h1>Post {`${post_id}`}</h1>
-//           <button
-//             type="button"
-//             className="btn"
-//             onClick={handle_click}
-//             >Change Post</button>
-//         </div>
-//   );
-// }
-
+    return (<div className="posts-wrapper">
+        <h1>Nested Objects: CRUD for posts, comments and subcomments.</h1>
+    </div>)
+}
