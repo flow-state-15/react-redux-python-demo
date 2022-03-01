1e2596ba327272977365ccad3c4ebde1ed401114
flow-state-15 <purcellgroup88@gmail.com>
2022-03-01 16:55:42 -0600

    styling

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 
 
 export default function SplashPage(){
@@ -6,12 +7,12 @@ export default function SplashPage(){
         <div className="splash-wrapper">
             <div className="gif-blurb-wrap">
                 <h2>
-                    The gif animation demonstration displays the rendering power of the useSelector and useDispatch hooks.
+                    The <Link to="/gif-demo">gif</Link> animation demonstration displays the rendering power of the useSelector and useDispatch hooks.
                 </h2>
             </div>
             <div className="nesting-blurb-wrap">
                 <h2>
-                    The posts demonstration samples a much more complex scenario that involves nested objects in state. Dynamic rerendering is still achieved through only the useSelector and useDispatch hooks. 
+                    The <Link to="/posts-demo">posts</Link> demonstration samples a much more complex scenario that involves nested objects in state. Dynamic rerendering is still achieved through only the useSelector and useDispatch hooks.
                 </h2>
             </div>
         </div>
