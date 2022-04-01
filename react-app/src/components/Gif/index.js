c2866363ddcbe772d14986dfe3c7f7dd29daa331
flow-state-15 <purcellgroup88@gmail.com>
2022-04-01 08:54:04 -0500

    cleanup

--- react-app/src/components/Gif/index.js
+++ react-app/src/components/Gif/index.js
@@ -1,86 +1,88 @@
 
 import React from "react"
-// import { useDispatch, useSelector } from 'react-redux';
-// import { increment_frame } from '../../store/gif'
+import { useDispatch, useSelector } from 'react-redux';
+import { increment_frame } from '../../store/gif'
 
 import frame_array from '../../assets/frames'
 
 //lets pretend this is an object you imported or something
-const bad_ref = { index: 0 }
+// const bad_ref = { index: 0 }
 
 export default function Gif() {
 
   //TODO: step 1. start here using Redux. useSelector alone will cause rerenders
-  // const dispatch = useDispatch();
-  //state var as primitive
-  // const gif_frame = useSelector((state) => state.gif?.frame?.frame_index);
+  const dispatch = useDispatch();
+  //note: state var is a primitive; makes it guaranteed to rerender
+  const gif_frame = useSelector((state) => state.gif?.frame?.frame_index);
 
   //edge case
-  // const image = frame_array[0]
+  const image = frame_array[0]
+
+  const manual_click = () => {
+    dispatch(increment_frame());
+  }
 
 
-  //TODO: step 2. the same react principles apply using local component state
-  //same effect but with component state instead
+  // //TODO: step 2. the same react principles apply using local component state
+  // //same effect but with local component state instead
+  // // todo: note the use of a callback in useState. it fixes race conditions
+
   // const [compState, setCompState] = React.useState(0)
 
+  //   const manual_click = () => {
 
-    // const manual_click = () => {
-      //todo: use redux and backend for frames:
-      // dispatch(increment_frame());
-
-      //todo: use local component state
-      // setCompState(prev => {
-      //   if(prev < frame_array.length-1){
-      //     return prev + 1
-      //   } else return 0
-      // })
-    // };
+  //     setCompState(prev => {
+  //       if(prev < frame_array.length-1){
+  //         return prev + 1
+  //       } else return 0
+  //     })
+  //   };
 
 
 
   //todo: HERE BE DRAGONS! ->  using state improperly
-  //react component lifecycle is based on object equality. react will do a === on state to decide when to rerender
-  //recall the gotchas you learned when doing a === on objects
+  // //react component lifecycle is based on object equality. react will do a === on state to decide when to rerender
+  // //recall the gotchas you learned when doing a === on objects
 
-  const [compState, setCompState] = React.useState(bad_ref)
+  // const [compState, setCompState] = React.useState(bad_ref)
 
-  const manual_click = () => {
-    //scenario 1:
-    //dont modify objects outside of react state!!
-    bad_ref.index = bad_ref.index + 1
-    setCompState(bad_ref)
+  // const manual_click = () => {
+  //   //scenario 1:
+  //   //dont modify objects outside of react state!!
+  //   bad_ref.index = bad_ref.index + 1
+  //   setCompState(bad_ref)
 
-    //scenario 2:
-    // setCompState(state => {
-    //   console.log("running useState callback!", state)
-    //   state.index += 1
-    //   const modified_state = state
-    //   console.log("same object in memory? ", state === modified_state)
-    //   return modified_state
-    // })
+  //   //scenario 2:
+  //   // setCompState(state => {
+  //   //   console.log("running useState callback!", state)
+  //   //   state.index += 1
+  //   //   const modified_state = state
+  //   //   console.log("same object in memory? ", state === modified_state)
+  //   //   return modified_state
+  //   // })
 
-    //bad reference: we are mutating an existing object
-    //reference in memory is the same
-    //react runs oldState === newState and it is true, so nothing happens
+  //   //bad reference: we are mutating an existing object
+  //   //reference in memory is the same
+  //   //react runs oldState === newState and it is true, so nothing happens
 
 
-    //todo: how to fix this
-    //copy old state and return a new reference in memory
-    //the logs will show the asynchronous nature of useState hook
-    // setCompState(state => {
+  //   //todo: how to fix this
+  //   //copy old state and return a new reference in memory
+  //   //the logs will show the asynchronous nature of useState hook
+  //   // setCompState(state => {
 
-    //   console.log("logging previous state:: ", state)
-    //   const modified_state = {...state, index: state.index + 1}
-    //   console.log("same object in memory? ", state === modified_state)
+  //   //   console.log("logging previous state:: ", state)
+  //   //   const modified_state = {...state, index: state.index + 1}
+  //   //   console.log("same object in memory? ", state === modified_state)
 
-    //   return modified_state
-    // })
-    console.log("I run before the useState callback!", compState)
-  }
+  //   //   return modified_state
+  //   // })
+  //   console.log("I run before the useState callback!", compState)
+  // }
 
-  React.useEffect(() => {
-    console.log("useEffect runs before render so you can see the change!", compState)
-  }, [compState]);
+  // React.useEffect(() => {
+  //   console.log("useEffect runs before render so you can see the change!", compState)
+  // }, [compState]);
 
 
   //TODO for good times
@@ -124,19 +126,17 @@ export default function Gif() {
         className="img-gif"
         //TODO: here we can use either redux or component state to source the index
 
-
-        //todo: component state source
         //from bad_ref:
-        src={frame_array[compState.index]}
+        // src={frame_array[compState.index]}
 
         //from redux:
-        // src={
-        //   gif_frame
-        //   ? frame_array[gif_frame]
-        //   : image
-        // }
+        src={
+          gif_frame
+          ? frame_array[gif_frame]
+          : image
+        }
 
-        //from local manual:
+        //from local state:
         // src={frame_array[compState]}
 
         //from local auto:
@@ -153,7 +153,9 @@ export default function Gif() {
       <br/>
       <br/>
       {/* //todo: switch up the var depending on your source */}
-      <span> frame: {compState.index} </span>
+      <span> Using a redux useSelector!  Frame: {gif_frame} </span>
+      {/* <span> Using local useState!  Frame: {compState} </span> */}
+      {/* <span> Using a bad object reference!  Frame: {compState.index} </span> */}
     </div>
   );
 }
